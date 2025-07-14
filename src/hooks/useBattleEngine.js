60e0e8db571f678d8020a017059a61fe5dfc119c
ninjaman0722 <ninjaman0722@gmail.com>
2025-07-14 12:39:58 -0500

    Update useBattleEngine.js

--- src/hooks/useBattleEngine.js
+++ src/hooks/useBattleEngine.js
@@ -329,7 +329,7 @@ export const useBattleEngine = (battleState, battleId, allTrainers, queuedAction
         });
     };
 
-
+// this is a test comment
     const runEndOfTurnPhase = (currentBattleState, newLog) => {
         const { teams, activePokemonIndices, field } = currentBattleState;
 
